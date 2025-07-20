4cad3e8ac8a2aa09114d49c9dc2e9c96f9735cc5
TrinhSuong <suong0344887316@gmail.com>
2025-07-21 02:04:36 +0700

    Khoi tao du an

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -4,16 +4,12 @@
       <InputSearch v-model="searchText" />
     </div>
     <div class="mt-3 col-md-6">
-      <h4>
-        Danh bạ
-        <i class="fas fa-address-book"></i>
-      </h4>
+      <h4>Danh bạ<i class="fas fa-address-book"></i></h4>
       <ContactList
         v-if="filteredContactsCount > 0"
         :contacts="filteredContacts"
         v-model:activeIndex="activeIndex"
-        @edit-id="editContact"
-      />
+        @edit-id="editContact"/>
       <p v-else>Không có liên hệ nào.</p>
       <div class="mt-3 row justify-content-around align-items-center">
         <button class="btn btn-sm btn-primary" @click="refreshList()">
@@ -29,20 +25,14 @@
     </div>
     <div class="mt-3 col-md-6">
       <div v-if="activeContact">
-        <h4>
-          Chi tiết Liên hệ
-          <i class="fas fa-address-card"></i>
-        </h4>
+        <h4>Chi tiết Liên hệ<i class="fas fa-address-card"></i></h4>
         <ContactCard :contact="activeContact" />
         <router-link
           :to="{
             name: 'contact.edit',
-            params: { id: activeContact._id },
-          }"
-        >
+            params: { id: activeContact._id },}">
           <span class="mt-2 badge badge-warning">
-            <i class="fas fa-edit"></i> Hiệu chỉnh</span
-          >
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span>
         </router-link>
       </div>
     </div>
@@ -55,11 +45,7 @@ import InputSearch from "@/components/InputSearch.vue";
 import ContactList from "@/components/ContactList.vue";
 import ContactService from "@/services/contact.service";
 export default {
-  components: {
-    ContactCard,
-    InputSearch,
-    ContactList,
-  },
+  components: {ContactCard, InputSearch, ContactList,},
   data() {
     return {
       contacts: [],
